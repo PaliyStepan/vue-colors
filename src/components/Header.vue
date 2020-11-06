e005eaab6d8cd35e4790f18cf742d927c0423c47
Paliy Stepan <palstepweb@gmail.com>
2020-11-06 21:37:52 +0300

    header add adaptive, and menu func show/hide

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,14 +3,16 @@
         .container
             .header__inner
                 .header__burger
-                    .burger
+                    .burger(@click="handleMenu")
                         .burger__line.burger__line_top
                         .burger__line.burger__line_center
                         .burger__line.burger__line_bottom
                 .header__left
-                    a.logo
+                    a.header__logo
                         img(src="../assets/img/logo.svg")
-                .header__menu
+                .header__menu(:class="{active : showMenu}")
+                    .close-menu(@click="handleMenu")
+                        img(src="../assets/img/close.svg")
                     nav.header__nav.nav
                         ul.nav__list
                             router-link.nav__item(v-for="link in links" tag="li" :to="link.url" active-class="nav__item_active")
@@ -42,6 +44,7 @@
     export default {
         name: "Header",
         data:()=>({
+            showMenu: false,
             links: [
                 {title: 'Продукты', url: '/products'},
                 {title: 'Цвета', url: '/colors'},
@@ -49,8 +52,23 @@
                 {title: 'Советы', url: '/advices'},
                 {title: 'Найти магазин', url: '/find'},
             ],
-
-        })
+        }),
+        methods: {
+            handleMenu(){
+                this.showMenu = !this.showMenu;
+                console.log('rew')
+            },
+            // handleView(){
+            //     this.mobileView = window.innerWidth <= 1199;
+            // }
+        },
+        // created() {
+        //     this.handleView();
+        //     window.addEventListener("resize", this.handleView);
+        // },
+        // destroyed() {
+        //     window.removeEventListener("resize", this.handleView);
+        // },
     }
 </script>
 
@@ -163,6 +181,125 @@
         }
         &__img {
             width: 22px;
+        }
+    }
+
+    .close-menu{
+        display: none;
+    }
+
+    @media only screen and (max-width: 1366px){
+        .header{
+            &__phone {
+                margin-right: 60px;
+            }
+        }
+    }
+
+    @media only screen and (max-width: 1199px){
+        .header{
+            &__inner {
+                justify-content: space-between;
+            }
+            &__left {
+                padding-right: 0;
+                width: auto;
+                min-width: auto;
+            }
+            &__menu {
+                position: fixed;
+                z-index: 40;
+                top: 0;
+                right: auto;
+                left: -100%;
+                width: 100%;
+                background-color: #fff;
+                bottom: 0;
+                flex-grow: 0;
+                display: block;
+                margin: 0;
+                transition: left 0.3s ease-out;
+                overflow: auto;
+                padding: 60px 40px;
+                &.active {
+                    left: 0;
+                }
+            }
+            &__burger {
+                display: block;
+            }
+            &__logo {
+                display: block;
+                width: 100px;
+                img {
+                    width: 100%;
+                    max-width: 100%;
+                }
+            }
+            &__cart {
+                margin-left: 0;
+            }
+            &__phone {
+                margin-right: 0;
+                margin-bottom: 30px;
+            }
+        }
+
+        .close-menu{
+            display: flex;
+            position: absolute;
+            top: 30px;
+            right: 30px;
+            width: 35px;
+            height: 35px;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .nav {
+            margin-bottom: 30px;
+            &__list {
+                display: block;
+            }
+            &__item {
+                margin-right: 0;
+                margin-bottom: 20px;
+                &:last-of-type {
+                    margin-bottom: 0;
+                }
+            }
+            &__link {
+                font-size: 16px;
+            }
+        }
+
+        .burger {
+            width: 24px;
+            height: 14px;
+            position: relative;
+            &__line {
+                position: absolute;
+                width: 100%;
+                left: 0;
+                height: 2px;
+                background-color: $black;
+            }
+            &__line_top {
+                top: 0;
+            }
+            &__line_center {
+                top: 50%;
+                transform: translateY(-50%);
+            }
+            &__line_bottom {
+                bottom: 0;
+            }
+        }
+    }
+
+    @media only screen and (max-width: 1199px){
+        .header{
+            padding: 20px 0;
         }
     }
 
